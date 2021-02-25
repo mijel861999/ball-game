2708a461e7521a4d671ed61d0d0415a9ea3f16c3
Miguel Castillo <miguel.castillo861999@gmail.com>
2021-02-25 18:51:47 -0500

    Primer commit

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public float jumpForce = 2.0f;
+    public float speed = 3.0f;
+
+
+    private Rigidbody2D rgbd;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("OnGround", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rgbd.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rgbd.velocity.y) ;
+
+        if (Input.GetButtonDown("Jump")) {
+            rgbd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("OnGround", false);
+        }   
+        
+    }
+}
